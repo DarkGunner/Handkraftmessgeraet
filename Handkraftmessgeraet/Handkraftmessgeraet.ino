2895aa8846cfbb0a31345279855177f77f808cb2
Tim Heilmann <tim.heilmann@t-online.de>
2019-10-27 22:57:50 +0100

    Projektdateien hinzufügen.

---
+++ Handkraftmessgeraet/Handkraftmessgeraet.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Handkraftmessgeraet.ino
+ Created:	27.10.2019 22:54:56
+ Author:	Tim
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
